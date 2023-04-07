03dd2ce6c33180c668261cd4eb4aed25a989c161
Tom Tillistrand <tom.tillistrand@velir.com>
2023-04-07 18:05:11 -0400

    Add custom transform option

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ import {
 } from '@contentful/rich-text-types';
 import { parse } from 'html-to-ast';
 // import { flatMap } from 'lodash';
-import examples from './examples.js';
 
 type HtmlNodes = ReturnType<typeof parse>;
 type HtmlNode = HtmlNodes[0];
@@ -22,18 +21,26 @@ interface HtmlLinkNode extends HtmlNode {
   };
 }
 
+type Config = {
+  customTransform?: (node: HtmlNode) => Node | null;
+  preFilter?: (node: HtmlNode) => HtmlNode | boolean;
+};
+
 function isLink(node: HtmlNode): node is HtmlLinkNode {
   return node.name === 'a';
 }
 
-function richTextFromHtml(
-  html: string,
-  filter?: (node: HtmlNode) => HtmlNode | boolean,
-): Document {
+/**
+ * Convert HTML into Contentful rich text nodes
+ * @param html - HTML to convert to Contentful Rich Text document format
+ * @param config.customTransform - Callback to handle any HTML elements that you'd like to custom transform, or as a fallback for elements that don't have a corresponding Contentful rich text node. Return `null` for elements that you want pass through to the default transformer
+ * @param config.preFilter - Callback to modify the HTML AST prior to it being transformed to Contentful rich text nodes. This is useful for stripping out or modifying HTML elements, attributes, or text that you don't want to be transformed
+ */
+function richTextFromHtml(html: string, config: Config): Document {
   const ast = parse(html);
 
   let modifiedAst: HtmlNodes | undefined;
-  if (filter) modifiedAst = modifyAst(ast, filter);
+  if (config.preFilter) modifiedAst = modifyAst(ast, config.preFilter);
 
   console.log(
     util.inspect(modifiedAst ?? ast, {
@@ -74,25 +81,50 @@ function buildHyperlink(node: HtmlLinkNode): Hyperlink {
   return hyperlink;
 }
 
-function htmlToRichTextNode(node: HtmlNode) {
-  console.log(node);
+function htmlToRichTextNode(
+  node: HtmlNode,
+  customTransform?: Config['customTransform'],
+): Node {
+  let maybeCustomTransformedNode: Node | null | undefined;
+  if (customTransform) {
+    maybeCustomTransformedNode = customTransform(node);
+  }
+
+  // If maybeCustomTransformedNode is an object, user has returned
+  // a custom transformed node that we'll use instead
+  if (
+    typeof maybeCustomTransformedNode === 'object' &&
+    maybeCustomTransformedNode !== null
+  )
+    return maybeCustomTransformedNode;
+
   if (isLink(node)) {
     return buildHyperlink(node);
   }
+
+  // TODO: Add support for table elements
 }
 
-function htmlToRichTextNodes(nodes: HtmlNodes): Node[] {
+function htmlToRichTextNodes(
+  nodes: HtmlNodes,
+  customTransform?: Config['customTransform'],
+): Node[] {
   if (!nodes) {
     return [];
   }
 
   const rtNodes = nodes.map((node) => {
-    htmlToRichTextNode(node);
+    htmlToRichTextNode(node, customTransform);
   });
+
+  return rtNodes;
 }
 
-function astToRichTextDocument(ast: HtmlNodes): Document {
-  const content = htmlToRichTextNodes(ast);
+function astToRichTextDocument(
+  ast: HtmlNodes,
+  customTransform?: Config['customTransform'],
+): Document {
+  const content = htmlToRichTextNodes(ast, customTransform);
 
   return {
     nodeType: BLOCKS.DOCUMENT,
--- src/test.ts
+++ src/test.ts
@@ -1,15 +1,17 @@
 import richTextFromHtml from './index.js';
 import examples from './examples.js';
 
-richTextFromHtml(examples[0], (node) => {
-  const archivedLinkRegEx = /{page_\d+}/;
-  if (
-    node.type === 'tag' &&
-    node.name === 'a' &&
-    typeof node.attrs.href === 'string' &&
-    archivedLinkRegEx.test(node.attrs.href)
-  )
-    return false;
+richTextFromHtml(examples[0], {
+  filter: (node) => {
+    const archivedLinkRegEx = /{page_\d+}/;
+    if (
+      node.type === 'tag' &&
+      node.name === 'a' &&
+      typeof node.attrs.href === 'string' &&
+      archivedLinkRegEx.test(node.attrs.href)
+    )
+      return false;
 
-  return true;
+    return true;
+  },
 });
